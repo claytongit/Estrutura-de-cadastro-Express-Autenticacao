2bfb20b28927e79eba3124a3c8e095cab1a32315
Clayton Tavares <54451974+claytongit@users.noreply.github.com>
2020-02-01 14:44:06 -0300

    Add files via upload

---
+++ Autenticação/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+
+const app = express();
+
+mongoose.connect(
+  'mongodb://localhost:27017/register',
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  }  
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3001);
